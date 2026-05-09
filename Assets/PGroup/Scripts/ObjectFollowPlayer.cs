575c8a74f3bd9b9c45209565fe07fff641139757
NopparatPGroupDev <nopparat.pgroup@gmail.com>
2026-05-09 18:52:39 +0700

    Fix again

--- Assets/PGroup/Scripts/ObjectFollowPlayer.cs
+++ Assets/PGroup/Scripts/ObjectFollowPlayer.cs
@@ -12,7 +12,7 @@ namespace PGroup
 
         private void Start()
         {
-            if (Camera.main != null) player = Camera.main.transform.parent.parent;
+            if (Camera.main.transform.parent.parent != null) player = Camera.main.transform.parent.parent;
         }
         private void LateUpdate()
         {
